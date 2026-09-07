44a1cafa7cc40022491b03e43590238cdf9ebf30
Shubham Tomar <shubhamtomar@sovereignbyte.tech>
2026-09-07 18:21:16 +0530

    fix(build): define SOVEREIGN_STATIC for Windows static core library build

--- core_engine/include/sovereign_engine.h
+++ core_engine/include/sovereign_engine.h
@@ -9,7 +9,9 @@ extern "C" {
 #include <stdbool.h>
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-    #ifdef SOVEREIGN_EXPORTS
+    #if defined(SOVEREIGN_STATIC)
+        #define SOVEREIGN_API
+    #elif defined(SOVEREIGN_EXPORTS)
         #define SOVEREIGN_API __declspec(dllexport)
     #else
         #define SOVEREIGN_API __declspec(dllimport)
