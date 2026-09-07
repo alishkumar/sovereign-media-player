29eb8c9f8d81f671fc2ff10994ed5dfc847b791f
alish.kumar <alish2130@gmail.com>
2026-09-07 15:52:20 +0530

    chore(release): bump version to 2.1.0

--- build_open_core.py
+++ build_open_core.py
@@ -137,8 +137,8 @@ def bundle_app(universal_bin):
     <key>CFBundleInfoDictionaryVersion</key><string>6.0</string>
     <key>CFBundleName</key><string>Sovereign Media Player</string>
     <key>CFBundlePackageType</key><string>APPL</string>
-    <key>CFBundleShortVersionString</key><string>1.2.0</string>
-    <key>CFBundleVersion</key><string>1.2.0</string>
+    <key>CFBundleShortVersionString</key><string>2.1.0</string>
+    <key>CFBundleVersion</key><string>2.1.0</string>
     <key>LSMinimumSystemVersion</key><string>10.15</string>
     <key>NSHighResolutionCapable</key><true/>
     <key>NSPrincipalClass</key><string>NSApplication</string>
